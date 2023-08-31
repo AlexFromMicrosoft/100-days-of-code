167001d7d08020ae75f1d002fe73d0731190b266
Matheus <40328889+AlexFromMicrosoft@users.noreply.github.com>
2023-08-31 14:49:55 -0300

    commit

---
+++ day_07/challenges/challenge_5/challenge_5.py
@@ -0,0 +1,49 @@
+#Step 5
+from art import logo
+from art import stages
+from words import word_list
+from clear import clear
+import random
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(logo)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    clear()
+
+    if guess in display:
+        print(f"You already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = letter
+
+
+    if guess not in chosen_word:
+
+        print(f"The letter {guess} is not in the word. You lose a life")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
--- day_07/forca.py
+++ day_07/forca.py
@@ -1,15 +1,1 @@
-# Gerar uma palavra aleatória
 
-# Gerar linhas equivalentes ao numero de letras da palavra aleatoria
-
-# Pedir uma letra ao usuário
-
-# Certo ou errado?
-
-# Se certo a letra é colocada na sua posição
-
-# Se errado perde uma vida
-
-# Se a vida acaba perde se todas as linhas são preenchidas vence
-
-# Idependente de vitoria ou derrota, fim de jogo
